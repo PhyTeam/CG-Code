ba279d69a46cd522c091b72c12d961208432f2d5
PhyTeam <buibaphuc95@gmail.com>
2016-03-15 10:01:03 +0700

    Ver1

---
+++ CGExercise1/Main.cpp
@@ -0,0 +1,295 @@
+#include <iostream>
+#include <GL/glut.h>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include "Mesh.h"
+/* global varible */
+void *font = GLUT_BITMAP_8_BY_13;
+Mesh donut;
+
+int mouseX, mouseY;
+bool mouseLeftDown, mouseRightDown, mouseMiddleDown;
+
+GLfloat cameraDistance = -2;
+GLfloat tx, ty, rotx, roty;
+
+/* process menu option 'op' */
+void menu(int op) {
+
+	switch (op) {
+	case 'Q':
+	case 'q':
+		exit(0);
+	}
+}
+
+/* executed when a regular key is pressed */
+void keyboardDown(unsigned char key, int x, int y) {
+
+	switch (key) {
+	case 'Q':
+	case 'q':
+	case  27:   // ESC
+		exit(0);
+	}
+}
+
+/* executed when a regular key is released */
+void keyboardUp(unsigned char key, int x, int y) {
+
+}
+
+/* executed when a special key is pressed */
+void keyboardSpecialDown(int k, int x, int y) {
+
+}
+
+/* executed when a special key is released */
+void keyboardSpecialUp(int k, int x, int y) {
+
+}
+
+/* reshaped window */
+void reshape(int width, int height) {
+
+	glFrontFace(GL_CCW);
+	glEnable(GL_DEPTH_TEST);
+
+	//GLfloat fieldOfView = 90.0f;
+	//glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	//const float fHalfSize = 2;
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	////gluPerspective(fieldOfView, (GLfloat)width / (GLfloat)height, 0.1, 500.0);
+	////glOrtho(-fHalfSize, fHalfSize, -fHalfSize, fHalfSize, -1000, 1000);
+	//glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
+
+	// set viewport to be the entire window
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	// set perspective viewing frustum
+	float aspectRatio = (float)width / height;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glFrustum(-aspectRatio, aspectRatio, -1, 1, 0.1, 100);
+	gluPerspective(60.0f, (float)(width) / height, 1.0f, 1000.0f); // FOV, AspectRatio, NearClip, FarClip
+
+														  // switch to modelview matrix in order to set scene
+	glMatrixMode(GL_MODELVIEW);
+}
+
+/* executed when button 'button' is put into state 'state' at screen position ('x', 'y') */
+void mouseClick(int button, int state, int x, int y) {
+	mouseX = x;
+	mouseY = y;
+
+	if (button == GLUT_LEFT_BUTTON)
+	{
+		if (state == GLUT_DOWN)
+		{
+			mouseLeftDown = true;
+		}
+		else if (state == GLUT_UP)
+			mouseLeftDown = false;
+	}
+
+	else if (button == GLUT_RIGHT_BUTTON)
+	{
+		if (state == GLUT_DOWN)
+		{
+			mouseRightDown = true;
+		}
+		else if (state == GLUT_UP)
+			mouseRightDown = false;
+	}
+
+	else if (button == GLUT_MIDDLE_BUTTON)
+	{
+		if (state == GLUT_DOWN)
+		{
+			mouseMiddleDown = true;
+		}
+		else if (state == GLUT_UP)
+			mouseMiddleDown = false;
+	}
+}
+
+/* executed when the mouse moves to position ('x', 'y') */
+void mouseMotion(int x, int y) {
+	if (mouseLeftDown)
+	{
+		tx += (x - mouseX);
+		ty += (y - mouseY);
+		mouseX = x;
+		mouseY = y;
+	}
+	if (mouseRightDown)
+	{
+		cameraDistance += (y - mouseY) * 0.2f;
+		mouseY = y;
+	}
+
+	glutPostRedisplay();
+}
+
+/* struct vertex */
+class Vertex3D {
+public:
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+public:
+	Vertex3D() :
+		x(0.0f), y(0.0f), z(0.0f) {}
+	Vertex3D(GLfloat _x, GLfloat _y, GLfloat _z)
+		: x(_x), y(_y), z(_z)
+	{}
+
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// write 2d text using GLUT
+// The projection matrix must be set to orthogonal before call this function.
+///////////////////////////////////////////////////////////////////////////////
+void drawString(const char *str, int x, int y, float color[4], void *font)
+{
+	glPushAttrib(GL_LIGHTING_BIT | GL_CURRENT_BIT); // lighting and color mask
+	glDisable(GL_LIGHTING);     // need to disable lighting for proper text color
+
+	glColor4fv(color);          // set text color
+	glRasterPos2i(x, y);        // place text position
+
+								// loop all characters in the string
+	while (*str)
+	{
+		glutBitmapCharacter(font, *str);
+		++str;
+	}
+
+	glEnable(GL_LIGHTING);
+	glPopAttrib();
+}
+
+/* render the scene */
+void draw() {
+	glColor3ub(255, 255, 0);
+
+	// Create a sphere
+	donut.DrawColor();
+	/* render the scene here */
+
+	float color4[] = { 1,1,1,1 };
+	GLfloat mt[16];
+	glGetFloatv(GL_MODELVIEW_MATRIX, mt);
+	
+	glPushMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Draw info
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0, 800, 0, 600);
+
+	std::stringstream ss;
+	ss << std::fixed << std::setprecision(3);
+	int i = 0;
+	for (i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			ss << mt[i  + j * 4] << " ";
+		}
+		drawString(ss.str().c_str(), 10, 100 - 30 * i, color4, font);
+		ss.str("");
+	}
+	drawString("GL_MODELMATRIX", 10, 100 + 30, color4, font);
+
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glPopMatrix();
+}
+
+
+/* display callback function */
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear Z-buffer
+
+	// Set the camera orientation
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0, 0, 2, 0, 0, 0, 0, 1, 0);
+
+	// Rotate and zoom the camera. This order give you maya-like control
+	glTranslatef(0, 0, cameraDistance);
+	//glTranslatef(tx, ty, 0);
+	glRotatef(tx, 1, 0, 0);
+	glRotatef(ty, 0, 1, 0);
+
+	glPushMatrix();
+	draw();
+	glPopMatrix();
+	glFlush();
+	glutSwapBuffers();
+}
+
+/* executed when program is idle */
+void idle() {
+
+}
+
+/* initialize OpenGL settings */
+void initGL(int width, int height) {
+	// Create a mesh
+	donut.CreateDonut(1.0, 0.5);
+
+	reshape(width, height);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+}
+
+/* initialize GLUT settings, register callbacks, enter main loop */
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Perspective's GLUT Template");
+
+	// register glut call backs
+	glutKeyboardFunc(keyboardDown);
+	glutKeyboardUpFunc(keyboardUp);
+	glutSpecialFunc(keyboardSpecialDown);
+	glutSpecialUpFunc(keyboardSpecialUp);
+	glutMouseFunc(mouseClick);
+	glutMotionFunc(mouseMotion);
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	//glutIdleFunc(idle);
+	glutIgnoreKeyRepeat(true); // ignore keys held down
+
+							   // create a sub menu 
+	int subMenu = glutCreateMenu(menu);
+	glutAddMenuEntry("Do nothing", 0);
+	glutAddMenuEntry("Really Quit", 'q');
+
+	// create main "right click" menu
+	//glutCreateMenu(menu);
+	//glutAddSubMenu("Sub Menu", subMenu);
+	//glutAddMenuEntry("Quit", 'q');
+	//glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	initGL(800, 600);
+
+	glutMainLoop();
+	return 0;
+}
---
+++ CGExercise1/Mesh.cpp
@@ -0,0 +1,523 @@
+#include "Mesh.h"
+#include <math.h>
+
+#define PI			3.1415926
+#define	COLORNUM		14
+
+
+float	ColorArr[COLORNUM][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, { 0.0,  0.0, 1.0}, 
+								{1.0, 1.0,  0.0}, { 1.0, 0.0, 1.0},{ 0.0, 1.0, 1.0}, 
+								 {0.3, 0.3, 0.3}, {0.5, 0.5, 0.5}, { 0.9,  0.9, 0.9},
+								{1.0, 0.5,  0.5}, { 0.5, 1.0, 0.5},{ 0.5, 0.5, 1.0},
+									{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}};
+
+
+
+
+
+void Mesh::CreateCube(float	fSize)
+{
+	int i;
+
+	numVerts=8;
+	pt = new Point3[numVerts];
+	pt[0].set(-fSize, fSize, fSize);
+	pt[1].set( fSize, fSize, fSize);
+	pt[2].set( fSize, fSize, -fSize);
+	pt[3].set(-fSize, fSize, -fSize);
+	pt[4].set(-fSize, -fSize, fSize);
+	pt[5].set( fSize, -fSize, fSize);
+	pt[6].set( fSize, -fSize, -fSize);
+	pt[7].set(-fSize, -fSize, -fSize);
+
+
+	numFaces= 6;
+	face = new Face[numFaces];
+
+	//Left face
+	face[0].nVerts = 4;
+	face[0].vert = new VertexID[face[0].nVerts];
+	face[0].vert[0].vertIndex = 1;
+	face[0].vert[1].vertIndex = 5;
+	face[0].vert[2].vertIndex = 6;
+	face[0].vert[3].vertIndex = 2;
+	for(i = 0; i<face[0].nVerts ; i++)
+		face[0].vert[i].colorIndex = 0;
+	
+	//Right face
+	face[1].nVerts = 4;
+	face[1].vert = new VertexID[face[1].nVerts];
+	face[1].vert[0].vertIndex = 0;
+	face[1].vert[1].vertIndex = 3;
+	face[1].vert[2].vertIndex = 7;
+	face[1].vert[3].vertIndex = 4;
+	for(i = 0; i<face[1].nVerts ; i++)
+		face[1].vert[i].colorIndex = 1;
+
+	//top face
+	face[2].nVerts = 4;
+	face[2].vert = new VertexID[face[2].nVerts];
+	face[2].vert[0].vertIndex = 0;
+	face[2].vert[1].vertIndex = 1;
+	face[2].vert[2].vertIndex = 2;
+	face[2].vert[3].vertIndex = 3;
+	for(i = 0; i<face[2].nVerts ; i++)
+		face[2].vert[i].colorIndex = 2;
+
+	//bottom face
+	face[3].nVerts = 4;
+	face[3].vert = new VertexID[face[3].nVerts];
+	face[3].vert[0].vertIndex = 7;
+	face[3].vert[1].vertIndex = 6;
+	face[3].vert[2].vertIndex = 5;
+	face[3].vert[3].vertIndex = 4;
+	for(i = 0; i<face[3].nVerts ; i++)
+		face[3].vert[i].colorIndex = 3;
+
+	//near face
+	face[4].nVerts = 4;
+	face[4].vert = new VertexID[face[4].nVerts];
+	face[4].vert[0].vertIndex = 4;
+	face[4].vert[1].vertIndex = 5;
+	face[4].vert[2].vertIndex = 1;
+	face[4].vert[3].vertIndex = 0;
+	for(i = 0; i<face[4].nVerts ; i++)
+		face[4].vert[i].colorIndex = 4;
+
+	//Far face
+	face[5].nVerts = 4;
+	face[5].vert = new VertexID[face[5].nVerts];
+	face[5].vert[0].vertIndex = 3;
+	face[5].vert[1].vertIndex = 2;
+	face[5].vert[2].vertIndex = 6;
+	face[5].vert[3].vertIndex = 7;
+	for(i = 0; i<face[5].nVerts ; i++)
+		face[5].vert[i].colorIndex = 5;
+
+}
+
+
+void Mesh::CreateTetrahedron()
+{
+	int i;
+	numVerts=4;
+	pt = new Point3[numVerts];
+	pt[0].set(0, 0, 0);
+	pt[1].set(1, 0, 0);
+	pt[2].set(0, 1, 0);
+	pt[3].set(0, 0, 1);
+
+	numFaces= 4;
+	face = new Face[numFaces];
+
+	face[0].nVerts = 3;
+	face[0].vert = new VertexID[face[0].nVerts];
+	face[0].vert[0].vertIndex = 1;
+	face[0].vert[1].vertIndex = 2;
+	face[0].vert[2].vertIndex = 3;
+	for(i = 0; i<face[0].nVerts ; i++)
+		face[0].vert[i].colorIndex = 0;
+	
+
+	face[1].nVerts = 3;
+	face[1].vert = new VertexID[face[1].nVerts];
+	face[1].vert[0].vertIndex = 0;	
+	face[1].vert[1].vertIndex = 2;
+	face[1].vert[2].vertIndex = 1;
+	for(i = 0; i<face[1].nVerts ; i++)
+		face[1].vert[i].colorIndex = 1;
+
+	
+	face[2].nVerts = 3;
+	face[2].vert = new VertexID[face[2].nVerts];
+	face[2].vert[0].vertIndex = 0;
+	face[2].vert[1].vertIndex = 3;
+	face[2].vert[2].vertIndex = 2;
+	for(i = 0; i<face[2].nVerts ; i++)
+		face[2].vert[i].colorIndex = 2;
+
+
+	face[3].nVerts = 3;
+	face[3].vert = new VertexID[face[3].nVerts];
+	face[3].vert[0].vertIndex = 1;
+	face[3].vert[1].vertIndex = 3;
+	face[3].vert[2].vertIndex = 0;
+	for(i = 0; i<face[3].nVerts ; i++)
+		face[3].vert[i].colorIndex = 3;
+}
+
+void Mesh::CreateCuboid(float	fSizeX, float fSizeY, float fSizeZ)
+{
+	int i;
+
+	numVerts = 8;
+	pt = new Point3[numVerts];
+	pt[0].set(-fSizeX, fSizeY, fSizeZ);
+	pt[1].set(fSizeX, fSizeY, fSizeZ);
+	pt[2].set(fSizeX, fSizeY, -fSizeZ);
+	pt[3].set(-fSizeX, fSizeY, -fSizeZ);
+	pt[4].set(-fSizeX, -fSizeY, fSizeZ);
+	pt[5].set(fSizeX, -fSizeY, fSizeZ);
+	pt[6].set(fSizeX, -fSizeY, -fSizeZ);
+	pt[7].set(-fSizeX, -fSizeY, -fSizeZ);
+
+
+	numFaces = 6;
+	face = new Face[numFaces];
+
+	//Left face
+	face[0].nVerts = 4;
+	face[0].vert = new VertexID[face[0].nVerts];
+	face[0].vert[0].vertIndex = 1;
+	face[0].vert[1].vertIndex = 5;
+	face[0].vert[2].vertIndex = 6;
+	face[0].vert[3].vertIndex = 2;
+	for (i = 0; i<face[0].nVerts; i++)
+		face[0].vert[i].colorIndex = 0;
+
+	//Right face
+	face[1].nVerts = 4;
+	face[1].vert = new VertexID[face[1].nVerts];
+	face[1].vert[0].vertIndex = 0;
+	face[1].vert[1].vertIndex = 3;
+	face[1].vert[2].vertIndex = 7;
+	face[1].vert[3].vertIndex = 4;
+	for (i = 0; i<face[1].nVerts; i++)
+		face[1].vert[i].colorIndex = 1;
+
+	//top face
+	face[2].nVerts = 4;
+	face[2].vert = new VertexID[face[2].nVerts];
+	face[2].vert[0].vertIndex = 0;
+	face[2].vert[1].vertIndex = 1;
+	face[2].vert[2].vertIndex = 2;
+	face[2].vert[3].vertIndex = 3;
+	for (i = 0; i<face[2].nVerts; i++)
+		face[2].vert[i].colorIndex = 2;
+
+	//bottom face
+	face[3].nVerts = 4;
+	face[3].vert = new VertexID[face[3].nVerts];
+	face[3].vert[0].vertIndex = 7;
+	face[3].vert[1].vertIndex = 6;
+	face[3].vert[2].vertIndex = 5;
+	face[3].vert[3].vertIndex = 4;
+	for (i = 0; i<face[3].nVerts; i++)
+		face[3].vert[i].colorIndex = 3;
+
+	//near face
+	face[4].nVerts = 4;
+	face[4].vert = new VertexID[face[4].nVerts];
+	face[4].vert[0].vertIndex = 4;
+	face[4].vert[1].vertIndex = 5;
+	face[4].vert[2].vertIndex = 1;
+	face[4].vert[3].vertIndex = 0;
+	for (i = 0; i<face[4].nVerts; i++)
+		face[4].vert[i].colorIndex = 4;
+
+	//Far face
+	face[5].nVerts = 4;
+	face[5].vert = new VertexID[face[5].nVerts];
+	face[5].vert[0].vertIndex = 3;
+	face[5].vert[1].vertIndex = 2;
+	face[5].vert[2].vertIndex = 6;
+	face[5].vert[3].vertIndex = 7;
+	for (i = 0; i<face[5].nVerts; i++)
+		face[5].vert[i].colorIndex = 5;
+
+}
+
+void Mesh::CreateCylinder(int nSegment, float fHeight, float fRadius) {
+	int i;
+
+	numVerts = nSegment * 2 + 2;
+	pt = new Point3[numVerts];
+	float phi = 0;
+	pt[nSegment * 2].set(0, fHeight / 2, 0);
+	pt[nSegment * 2 + 1].set(0, - fHeight / 2, 0);
+	for (int i = 0; i < nSegment; i++, phi += 2.0f * PI / nSegment)
+	{
+		float x, y, z;
+		x = fRadius * cos(phi);
+		z = fRadius * sin(phi);
+		y = fHeight / 2.0f;
+		pt[i].set(x, y, z);
+		pt[i + nSegment].set(x, -y, z);
+	}
+
+	numFaces = (nSegment+1) * 3;
+	face = new Face[numFaces];
+
+	for (i = 0; i <= nSegment; i++)
+	{
+		int t = i;
+		//Top face
+		face[i].nVerts = 3;
+		face[i].vert = new VertexID[face[i].nVerts];
+		face[i].vert[0].vertIndex = 2 * nSegment;
+		face[i].vert[1].vertIndex = (i) % nSegment;
+		face[i].vert[2].vertIndex = (i + 1) % nSegment;
+		for (int j = 0; j<face[i].nVerts; j++)
+			face[i].vert[j].colorIndex = rand() % NUMCOLORS;
+		t = i + nSegment + 1;
+		//Top face
+		face[t].nVerts = 3;
+		face[t].vert = new VertexID[face[t].nVerts];
+		face[t].vert[0].vertIndex = 2*nSegment + 1;
+		face[t].vert[1].vertIndex = i % nSegment + nSegment;
+		face[t].vert[2].vertIndex = (i+1)%nSegment + nSegment;
+		for (int j = 0; j<face[i].nVerts; j++)
+			face[t].vert[j].colorIndex = rand() % NUMCOLORS;
+	}
+
+	for (int i = 0; i < nSegment; i++)
+	{
+		int t = i;
+		t = i + 2 * (nSegment+1);
+		//Top face
+		face[t].nVerts = 4;
+		face[t].vert = new VertexID[face[t].nVerts];
+		face[t].vert[0].vertIndex = i;
+		face[t].vert[1].vertIndex = (i + 1) % nSegment;
+		face[t].vert[2].vertIndex = (i + 1) % nSegment + nSegment;
+		face[t].vert[3].vertIndex = i + nSegment;
+		for (int j = 0; j < face[i].nVerts; j++)
+			face[t].vert[j].colorIndex = rand() % NUMCOLORS;
+	}
+	
+}
+
+void Mesh::CreateSphere(int nSlides, int nStacks, float fRadius) {
+	//numVerts = nSlides * nStacks;
+	//pt = new Point3[numVerts];
+
+	//numFaces = numVerts;
+	//face = new Face[numFaces * 2];
+
+	//float const R = 1. / (float)(nSlides - 1);
+	//float const S = 1. / (float)(nStacks - 1);
+	//int t = 0, k = 0;
+
+	//for (int r = 0; r < nSlides; ++r) {
+	//	for (int s = 0; s < nStacks; ++s) {
+	//		float const y = sin(- 2 * PI + PI * r * R);
+	//		float const x = cos(2 * PI * s * S) * sin(PI * r * R);
+	//		float const z = sin(2 * PI * s * S) * sin(PI * r * R);
+
+	//		pt[t++].set(x * fRadius, y * fRadius, z * fRadius);
+	//		//push_indices(indices, sectors, r, s);
+	//		int curRow = r * nStacks;
+	//		int nextRow = (r + 1) * nStacks;
+
+	//		int n = k++;
+	//		face[n].nVerts = 3;
+	//		face[n].vert = new VertexID[face[n].nVerts];
+	//		face[n].vert[0].vertIndex = curRow + s;
+	//		face[n].vert[1].vertIndex = nextRow + s;
+	//		face[n].vert[2].vertIndex = nextRow + s + 1;
+	//		for (size_t z = 0; z < face[n].nVerts; z++)
+	//		{
+	//			face[n].vert[z].colorIndex = rand() % NUMCOLORS;
+	//		}
+
+	//		n = k++;
+	//		face[n].nVerts = 3;
+	//		face[n].vert = new VertexID[face[n].nVerts];
+	//		face[n].vert[0].vertIndex = curRow + s;
+	//		face[n].vert[1].vertIndex = nextRow + s + 1;
+	//		face[n].vert[2].vertIndex = curRow + s + 1;
+	//		for (size_t z = 0; z < face[n].nVerts; z++)
+	//		{
+	//			face[n].vert[z].colorIndex = rand() % NUMCOLORS;
+	//		}
+	//	}
+	//}
+	float radius = fRadius;
+	int nSlice = nSlides;
+	int nStack = nStacks;
+	numVerts = nSlice * (nStack + 1);
+	numFaces = nSlice * nStack;
+	pt = new Point3[numVerts];
+	face = new Face[numFaces];
+	int i = 0;
+	float phir, thetar, x, y, z;
+	for (i = 0; i < nSlice; i++) {
+		phir = 2 * PI*i / nSlice;
+		for (int j = 0; j <= nStack; j++) {
+			thetar = PI / 2 - PI*j / nStack;
+			x = radius*cos(phir)*cos(thetar);
+			y = radius*sin(phir)*cos(thetar);
+			z = radius*sin(thetar);
+			pt[i*(nStack + 1) + j].set(z, y, x);
+		}
+	}
+
+	for (i = 0; i < nSlice; i++) {
+		for (int j = 0; j < nStack; j++) {
+			face[i*nStack + j].nVerts = 4;
+			face[i*nStack + j].vert = new VertexID[4];
+			face[i*nStack + j].vert[0].vertIndex = i*(nStack + 1) + j + 1;
+			face[i*nStack + j].vert[1].vertIndex = i*(nStack + 1) + j;
+			face[i*nStack + j].vert[2].vertIndex = (i == nSlice - 1) ? j : (i + 1)*(nStack + 1) + j;
+			face[i*nStack + j].vert[3].vertIndex = (i == nSlice - 1) ? j + 1 : (i + 1)*(nStack + 1) + j + 1;
+			for (int k = 0; k < face[i*nStack + j].nVerts; k++)
+				face[i*nStack + j].vert[k].colorIndex = k%COLORNUM;
+		}
+	}
+}
+
+void Mesh::CreateModel(float d, float R1, float R2, float fHeight) {
+	float xO1 = - R1 / (R1 + R2) * d;
+	float xO2 = R2 / (R1 + R2) * d;
+
+	//float phi = acos((R1 - R2) / d);
+	float phi = PI / 2;
+	int N = 10;
+	float step1 = (2 * PI - 2 * phi) / N;
+	float step2 = (2 * phi) / N;
+	numVerts = 2 * 2 * N;
+	pt = new Point3[numVerts];
+	int t = 0;
+	for (float i = phi; i < 2 * PI - phi; i += step1)
+	{
+		float x = R1 * cos(phi) + xO1;
+		float z = R1 * sin(phi);
+		float y = fHeight / 2;
+		pt[t++].set(x, y, z);
+	}
+
+	for (float i = -phi; i < phi; i+=step2)
+	{
+		float x = R2 * cos(phi) + xO2;
+		float z = R2 * sin(phi);
+		float y = fHeight / 2;
+		pt[t++].set(x, y, z);
+	}
+
+	for (int i = 0; i < 2 * N; i++)
+	{
+		pt[t++].set(pt[i].x, -pt[i].y, pt[i].z);
+	}
+
+	// Init face
+	numFaces = 2 * N;
+	face = new Face[numFaces];
+	for (int i = 0; i < 2 * N; i++)
+	{
+		face[i].nVerts = 4;
+		face[i].vert = new VertexID[face[i].nVerts];
+		face[i].vert[0].vertIndex = i;
+		face[i].vert[1].vertIndex = i + 1;
+		face[i].vert[2].vertIndex = i + 1 + 2 * N;
+		face[i].vert[3].vertIndex = i + 2 * N;
+		for (int j = 0; j < face[i].nVerts; j++)
+			face[i].vert[j].colorIndex = rand() % NUMCOLORS;
+	}
+}
+
+
+void Mesh::CreateDonut(GLfloat R, GLfloat d) {
+	int n = 100;
+	GLfloat step = 360.0f / n;
+	GLfloat x, y, z;
+	GLfloat** arr = new GLfloat*[n];
+	int t = 0;
+	for (GLfloat i = 0.0f; i < 360.0f; i += step)
+	{
+		x = d * cos(i * PI / 180) + R + d / 2;
+		y = d * sin(i * PI / 180) + R + d / 2;
+		z = 0;
+		arr[t] = new GLfloat[3];
+		arr[t][0] = x;
+		arr[t][1] = y;
+		arr[t][2] = z;
+		t++;
+	}
+	t = 0;
+
+	numVerts = n * n;
+	pt = new Point3[numVerts];
+	int m = 0;
+
+	for (GLfloat k = 0.0f; k < 360.0f; k += step)
+	{
+		for (int j = 0; j < n ; j++) {
+			GLfloat* xyz = arr[j];
+			GLfloat R1 = xyz[0];
+			GLfloat y = xyz[1];
+			GLfloat x = R1 * cos(k * PI / 180);
+			z = R1 * sin(k * PI / 180);
+			pt[m++].set(x, y, z);
+		}
+	}
+	numFaces = n * n;
+	face = new Face[numFaces];
+
+	t = 0;
+	for (int i = 0; i < n ; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			face[t].nVerts = 4;
+			face[t].vert = new VertexID[4];
+			face[t].vert[0].vertIndex = j + n*i;
+			face[t].vert[1].vertIndex = (j + 1) % n +  n*i;
+			face[t].vert[2].vertIndex = ((j+1)%n + n*((i + 1)% n));
+			face[t].vert[3].vertIndex = j + n * ((i + 1) % n );
+			for (int k = 0; k < face[t].nVerts; k++)
+				face[t].vert[k].colorIndex = k%COLORNUM;
+			t++;
+		}
+	}
+}
+
+void Mesh::DrawWireframe()
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	for (int f = 0; f < numFaces; f++)
+	{
+		glBegin(GL_POLYGON);
+		for (int v = 0; v < face[f].nVerts; v++)
+		{
+			int		iv = face[f].vert[v].vertIndex;
+
+			glVertex3f(pt[iv].x, pt[iv].y, pt[iv].z);
+		}
+		glEnd();
+	}
+}
+
+void Mesh::DrawPoint()
+{
+	glPointSize(2);
+	for (int f = 0; f < numFaces; f++)
+	{
+		glBegin(GL_POINT);
+		glColor3f(1, 1, 0);
+		glVertex3f(pt[f].x, pt[f].y, pt[f].z);
+		glEnd();
+	}
+}
+
+void Mesh::DrawColor()
+{
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	for (int f = 0; f < numFaces; f++)
+	{
+		glBegin(GL_POLYGON);
+		for (int v = 0; v < face[f].nVerts; v++)
+		{
+			int		iv = face[f].vert[v].vertIndex;
+			int		ic = face[f].vert[v].colorIndex;
+			
+			ic = f % COLORNUM;
+
+			glColor3f(ColorArr[ic][0], ColorArr[ic][1], ColorArr[ic][2]); 
+			glVertex3f(pt[iv].x, pt[iv].y, pt[iv].z);
+		}
+		glEnd();
+	}
+}
+
+
+
